10afa9834256bbdda995a548d41a7cba871601e9
Hugo Demenez <hdemenez@hotmail.fr>
2021-10-30 15:20:56 +0200

    commit

--- py/database.py
+++ py/database.py
@@ -1,18 +1,14 @@
-import mysql-connector
+import datetime
+import mysql.connector
 
-class database:
-    def __init__(self):
-        self.database = mysql.connector.connect(
-        host="localhost",
-        user="hugodemenez",
-        password="password",
-        database="website",
-        auth_plugin='mysql_native_password',
-        )
-        self.cursor = self.database.cursor()
-        
-database = database() 
-data = database.cursor()       
-database.data.execute("SELECT * FROM website")
-for values in data:
-  print(values)
+cnx = mysql.connector.connect(user='hugodemenez', database='database',password='password')
+cursor = cnx.cursor()
+
+query = ("INSERT INTO portfolio values ('bank',30,36)")
+
+
+cursor.execute(query)
+cursor.close()
+
+cnx.commit()
+cnx.close()
